51eecd7f5afd76afc99c2960ae84b4ada17e5b03
Volodymyr Havryliuk <chotkiypaca1@gmail.com>
2024-03-18 20:43:27 +0200

    Created docstrings for each function

--- app/io/input.py
+++ app/io/input.py
@@ -1,10 +1,52 @@
 def read_text_from_console():
+    """
+    Reads data from console.
+
+    Examples:
+    >>> read_text_from_console()
+    write your value: 21
+    21
+    >>> read_text_from_console()
+    write your value: 12
+    12
+    Returns:
+        str: The user's input value.
+    """
     pass
 
 
-def read_text_from_file_by_python_tools():
+def read_text_from_file_by_python_tools(filename):
+    """
+    Read data from file by built-in Python tools.
+    Examples:
+    >>> read_text_from_file_by_python_tools("text/hello.txt")
+    "hello, python!"
+    >>> read_text_from_file_by_python_tools("text/empty.txt")
+    ""
+    Args:
+        filename (str): The name of file to read from.
+    Returns:
+        str: The content of the file.
+    Raises:
+        FileNotFoundError: if programm is unable to locate the file.
+    """
     pass
 
 
 def read_text_from_file_by_pandas():
+    """
+    Read data from file as a table by tools of the pandas library.
+    Examples:
+    >>> read_text_from_file_by_pandas("text/pandas_first.txt")
+            "Name  Age  Height
+        0   John   25     170
+        1  Alice   28     165
+        2    Bob   30     180"
+    >>> read_text_from_file_by_pandas("text/empty.txt")
+    ""
+    Returns:
+        str: The content of the file as a table.
+    Raises:
+        FileNotFoundError: if programm is unable to locate the file.
+    """
     pass
--- app/io/output.py
+++ app/io/output.py
@@ -1,6 +1,36 @@
-def write_text_to_console():
+def write_text_to_console(text):
+    """
+    write text from a parameter to console.
+    Examples:
+    >>> write_text_to_console("hello, python!")
+    "hello, python!"
+    >>> write_text_to_console("victory!")
+    "victory"
+    >>> write_text_to_console("")
+    ""
+    Args:
+        text (str): The data to write to the console.
+    Returns:
+        None.
+    """
     pass
 
 
-def write_text_to_file():
+def write_text_to_file(text, filename):
+    """
+    write text from a parameter to a file, which name
+    is specified in filename parameter.
+    If the file does not exist, it will be created.
+    Examples:
+    >>> expected_text = "hello, python!"
+    >>> new_filename = "text/new.txt"
+    >>> write_text_to_file(expected_text, new_filename)
+    >>> actual_text = read_text_from_file_by_python_tools(new_filename)
+    >>> assert actual_text == expected_text
+    Args:
+        text (str): The text to write to the console.
+        filename (str): The name of the file to write the text.
+    Returns:
+        None.
+    """
     pass
