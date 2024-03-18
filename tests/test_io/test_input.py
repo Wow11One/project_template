6f493dba011b4c93745436c9f070bfa1e79147ea
Volodymyr Havryliuk <chotkiypaca1@gmail.com>
2024-03-18 22:36:49 +0200

    Added unit tests for the third function of an input.py file "read_text_from_file_by_pandas"

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -1,5 +1,8 @@
 import unittest
 
+from pandas import DataFrame
+
+from app.io.input import *
 from app.io.output import *
 
 
@@ -15,10 +18,40 @@ class TestInput(unittest.TestCase):
         initial_text = "test"
         new_filename = "text/new.txt"
         write_text_to_file(initial_text, new_filename)  # create a new file and insert text
-        text = read_text_from_file("text/new.txt")  # read inserted text
+        text = read_text_from_file(new_filename)  # read inserted text
         self.assertEqual(initial_text, text)
 
     def test_read_text_from_file_fails(self):
         nonexistent_filename = "text/nonexistent.txt"
         # assert that if file does not exist method raises an error
         self.assertRaises(FileNotFoundError, read_text_from_file, nonexistent_filename)
+
+    # tests for the third function of an input.py file "read_text_from_file_by_pandas"
+    # that returns table-like text from a file by pandas library
+
+    def test_read_text_from_file_by_pandas_successful(self):
+        initial_text = """John  25  170
+                          Alice 28  165
+                          Bob   30  180"""
+        pandas_filename = "text/pandas.txt"
+        write_text_to_file(initial_text, pandas_filename)  # create a new file and insert text
+        table = read_text_from_file_by_pandas(pandas_filename)  # read inserted text
+        # because this function returns a new table,
+        # so strings are not equal
+        self.assertNotEqual(initial_text, DataFrame.to_string(table))
+        # we check if our method added headers that we needed
+        self.assertIn("Name", DataFrame.to_string(table))
+        self.assertIn("Age", DataFrame.to_string(table))
+        self.assertIn("Height", DataFrame.to_string(table))
+
+    def test_read_text_from_file_by_pandas_empty_successful(self):
+        empty_text = ""
+        empty_filename = "text/empty.txt"
+        write_text_to_file(empty_text, empty_filename)
+        empty_table = read_text_from_file_by_pandas(empty_filename)
+        self.assertTrue(empty_table.empty)
+
+    def test_read_text_from_file_by_pandas_fails(self):
+        nonexistent_filename = "text/nonexistent.txt"
+        # assert that if file does not exist method raises an error
+        self.assertRaises(FileNotFoundError, read_text_from_file_by_pandas, nonexistent_filename)
